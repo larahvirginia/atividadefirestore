1e3e65800840464681c1956574726052b221e41b
larahvirginia <74986642+larahvirginia@users.noreply.github.com>
2022-12-02 21:43:24 -0300

    Update App

---
+++ App.js
@@ -0,0 +1,10 @@
+import { NavigationContainer } from "@react-navigation/native";
+import RoutesCrud from "./RoutesCrud";
+
+export default function App() {
+	return (
+		<NavigationContainer>
+			<RoutesCrud></RoutesCrud>
+		</NavigationContainer>
+	);
+}
